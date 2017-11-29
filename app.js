003ba928d69322e4850e7f37a6c801bbf4c4571b
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-11-28 17:15:06 -0800

    small refactor on calculate damage and win conditions

--- app.js
+++ app.js
@@ -12,16 +12,12 @@ new Vue({
          this.monsterHealth = 100
      },
        attack: function() {
-         var max = 10;
-         var min = 3;
-         var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-         this.monsterHealth -= damage;
-
-         if(this.monsterHealth <= 0) {
-             alert('You won!');
-             this.gameIsRunning = false;
-             return
+         this.monsterHealth -= this.calculateDamage(3, 10);
+         if(this.checkWin()) {
+             return;
          }
+         this.playerHealth -= this.calculateDamage(5,12);
+         this.checkWin();
        },
        specialAttack: function() {
 
@@ -31,6 +27,27 @@ new Vue({
        },
        giveUp: function() {
 
+       },
+       calculateDamage: function(min, max) {
+           return Math.max(Math.floor(Math.random() * max) + 1, min)
+       },
+       checkWin: function() {
+           if(this.monsterHealth <= 0) {
+               if (confirm('You won! New Game?')) {
+                   this.startGame();
+               } else {
+                   this.gameIsRunning = false;
+               }
+               return true;
+           }else if (this.playerHealth <=0) {
+               if (confirm('You lost! New Game?')){
+                   this.startGame();
+               }else {
+                   this.gameIsRunning = false;
+               }
+               return true;
+           }
+           return false;
        }
    }
 });
